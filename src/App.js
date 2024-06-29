75417742c5c9b05bdfaded7e2abd72a067d907d6
lino tom <linotom04@gmail.com>
2024-06-29 22:01:05 +0530

    ViewAllProduct Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddProduct from './components/AddProduct';
 import SearchProduct from './components/SearchProduct';
+import ViewAllProduct from './components/ViewAllProduct';
 
 function App() {
   return (
     <div>
       <AddProduct/>
       <SearchProduct/>
+      <ViewAllProduct/>
     </div>
   );
 }
---
+++ src/components/ViewAllProduct.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+
+const ViewAllProduct = () => {
+    return (
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">category</th>
+                        <th scope="col">Image</th>
+                        <th scope="col">Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</td>
+                        <td>109.95</td>
+                        <td>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</td>
+                        <td>men's clothing</td>
+                        <td>https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg</td>
+                        <td> "rate": 3.9,
+                        "count": 120</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Mens Casual Premium Slim Fit T-Shirts </td>
+                        <td>22.3</td>
+                        <td>"Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.</td>
+                        <td>men's clothing</td>
+                        <td>https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg</td>
+                        <td> "rate": 4.1,
+                        "count": 259</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Mens Cotton Jacket</td>
+                        <td>55.99</td>
+                        <td>great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.</td>
+                        <td>men's clothing</td>
+                        <td>https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg</td>
+                        <td> "rate": 4.7,
+                        "count": 500</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Mens Casual Slim Fit</td>
+                        <td> 15.99</td>
+                        <td>The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.</td>
+                        <td>men's clothing</td>
+                        <td>https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg</td>
+                        <td> "rate": 2.1,
+                        "count": 430</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet</td>
+                        <td>695</td>
+                        <td>From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.</td>
+                        <td>jewelery</td>
+                        <td>https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg</td>
+                        <td> "rate": 4.6,
+                        "count": 400</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>Solid Gold Petite Micropave</td>
+                        <td>168</td>
+                        <td>Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.</td>
+                        <td>jewelery</td>
+                        <td>https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg</td>
+                        <td>"rate": 3.9,
+                        "count": 70</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">7</th>
+                        <td>WD 2TB Elements Portable External Hard Drive - USB 3.0 </td>
+                        <td>64</td>
+                        <td>USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system</td>
+                        <td>electronics</td>
+                        <td>https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg</td>
+                        <td>  "rate": 3.3,
+                        "count": 203</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">8</th>
+                        <td>Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket</td>
+                        <td>29.95</td>
+                        <td>100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON</td>
+                        <td>women's clothing</td>
+                        <td>https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg</td>
+                        <td>  "rate": 2.9,
+                        "count": 340</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">9</th>
+                        <td>Rain Jacket Women Windbreaker Striped Climbing Raincoats</td>
+                        <td>39.99</td>
+                        <td>Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.
+                        </td><td>women's clothing</td>
+                        <td>https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg</td>
+                        <td>"rate": 3.8,
+                        "count": 679</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">10</th>
+                        <td>Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin</td>
+                        <td>599</td>
+                        <td>21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz</td>
+                        <td>electronics</td>
+                        <td>https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg</td>
+                        <td> "rate": 2.9,
+                        "count": 250</td>
+                        
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    )
+}
+
+export default ViewAllProduct
