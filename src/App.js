633f33dd1bfc67f1b9b4fd4008bd95c54e8c1b31
lino tom <linotom04@gmail.com>
2024-06-29 21:09:53 +0530

    SearchProduct Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddProduct from './components/AddProduct';
+import SearchProduct from './components/SearchProduct';
 
 function App() {
   return (
     <div>
       <AddProduct/>
+      <SearchProduct/>
     </div>
   );
 }
