a40818baab77454946ef85eb1ad17475c336e7b9
lino tom <linotom04@gmail.com>
2024-06-29 21:08:20 +0530

    AddProduct Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AddProduct from './components/AddProduct';
 
 function App() {
   return (
     <div>
+      <AddProduct/>
     </div>
   );
 }
